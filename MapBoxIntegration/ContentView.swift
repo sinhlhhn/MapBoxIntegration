7993d6e3197f1452e30072c7249f989720eee1d0
Sinh Le <sinhlh>
2026-02-11 15:09:09 +0700

    Initial Commit

---
+++ MapBoxIntegration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapBoxIntegration
+//
+//  Created by SinhLH.AVI on 11/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapBoxIntegration/MapBoxIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapBoxIntegrationApp.swift
+//  MapBoxIntegration
+//
+//  Created by SinhLH.AVI on 11/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MapBoxIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
